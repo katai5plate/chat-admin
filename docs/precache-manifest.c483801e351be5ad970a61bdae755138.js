0f2e6e94f0cb9fe5d53e46afcb7dfb9359db3372
katai5plate <katai5plate.sub@gmail.com>
2019-10-31 00:00:48 +0900

    rebuild + set homepage url

---
+++ docs/precache-manifest.c483801e351be5ad970a61bdae755138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4698d6033f0889873ec27f3f34a70923",
+    "url": "/chat-admin/index.html"
+  },
+  {
+    "revision": "303bf0acc8d3d1313112",
+    "url": "/chat-admin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "251c9735a7bac0af2516",
+    "url": "/chat-admin/static/js/2.b7cf1d08.chunk.js"
+  },
+  {
+    "revision": "303bf0acc8d3d1313112",
+    "url": "/chat-admin/static/js/main.f0ad6027.chunk.js"
+  },
+  {
+    "revision": "862511722a4f7055d349",
+    "url": "/chat-admin/static/js/runtime-main.3e21510b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/chat-admin/static/media/logo.25bf045c.svg"
+  }
+]);
